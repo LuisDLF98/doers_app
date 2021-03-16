f9c5be53a080795daeffffe65be2846189ee7d3b
Ldelaf98 <ldelaf98@tamu.edu>
2021-03-16 13:28:21 -0500

    Details screen update

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class MyApp extends StatelessWidget {
         accentColor: color[50],
         backgroundColor: color[500],
         scaffoldBackgroundColor: color[500],
+        cardColor: color[300],
 
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
@@ -145,33 +146,71 @@ class _MyHomePageState extends State<MyHomePage> {
   void toDetails() { setState(() { pageNum = 3; }); }
 }
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
+  DetailsScreen({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  _DetailsScreen createState() => _DetailsScreen();
+}
+
+class _DetailsScreen extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
+
+    _selectDate(BuildContext context) async {
+      final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: widget.selectedDate,
+        firstDate: new DateTime.now(),
+        lastDate: new DateTime.now().add(new Duration(days: 30)),
+        builder: (context, child) {
+          return Theme(
+            data: ThemeData(
+              colorScheme: ColorScheme.highContrastLight(
+                primary: color[100],
+              ),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            child: child,
+          );
+        },
+      );
+      if (picked != null && picked != widget.selectedDate)
+        setState(() {
+         widget.selectedDate = picked;
+        });
+    }
+
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return color[100];
+      }
+      return color[200];
+    }
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Details Page'),
+        title: Text('Create Job'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
@@ -180,7 +219,7 @@ class DetailsScreen extends StatelessWidget {
               child: Card(
                 child: TextField(
                   decoration: const InputDecoration(
-                    icon: Icon(Icons.map),
+                    icon: Icon(Icons.location_pin),
                     hintText: 'Location of job',
                   ),
                 ),
@@ -191,7 +230,7 @@ class DetailsScreen extends StatelessWidget {
               child: Card(
                 child: TextField(
                   decoration: const InputDecoration(
-                    icon: Icon(Icons.map),
+                    icon: Icon(Icons.streetview),
                     hintText: 'Address of this job',
                   ),
                 ),
