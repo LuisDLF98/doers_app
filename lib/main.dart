464262495cf8ec4016941ac680cd1f76fb9890a1
LightRanger <littlezippers7@gmail.com>
2021-03-31 10:11:22 -0500

    Sync after rebase

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:doers_app/Screens/payment_screen.dart';
 import 'package:doers_app/Screens/messaging_screen.dart';
 import 'package:doers_app/Screens/navigation_screen.dart';
 import 'package:doers_app/Components/hex_colors.dart';
-import 'package:doers_app/screens/job_details_screen.dart';
+import 'package:doers_app/Screens/job_details_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
